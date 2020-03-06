2a3d67b517d67fb1691b0f3c4cd6e31f56e87f75
Frederik Kammel <vatbub123+github@googlemail.com>
2020-03-06 19:18:59 +0100

    Fixed the position of the license header

--- src/main/kotlin/logging/LoggingHandlers.kt
+++ src/main/kotlin/logging/LoggingHandlers.kt
@@ -1,3 +1,23 @@
+/*-
+ * #%L
+ * Smart Charge
+ * %%
+ * Copyright (C) 2016 - 2020 Frederik Kammel
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+
 package com.github.vatbub.smartcharge.logging
 
 import javafx.application.Platform
@@ -122,29 +142,9 @@ object LoggingHandlers {
                 try {
                     val textNode = Text(formatter.format(record))
                     textNode.style = if (record.level.intValue() >= Level.WARNING.intValue())
-                        "-fx-fill: RED;
-/*-
- * #%L
- * Smart Charge
- * %%
- * Copyright (C) 2016 - 2020 Frederik Kammel
- * %%
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- * 
- *      http://www.apache.org/licenses/LICENSE-2.0
- * 
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- * #L%
- */
-                    -fx - font - weight:bold;"
+                        "-fx-fill: RED;-fx - font - weight:bold;"
                     else
-                    "-fx-fill: BLACK;-fx-font-weight:normal;"
+                        "-fx-fill: BLACK;-fx-font-weight:normal;"
 
                     val textFlow = loggingTextFlow
                     if (delayLogUpdates || textFlow == null)
